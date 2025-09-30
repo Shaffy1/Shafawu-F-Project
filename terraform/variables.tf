32b58b0bfc1063b935cd3c92c63199d3699a9eff
Shaffy1 <sweetieshafaw301@gmail.com>
2025-09-30 22:51:31 +0000

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,23 @@
-variable "aws_region" {}
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
-variable "mp3_bucket_name" {}
+variable "mp3_bucket_name" {
+  description = "S3 bucket name for audio files"
+  type        = string
+}
 
-variable "db_table_name" {}
+variable "db_table_name" {
+  description = "DynamoDB table name"
+  type        = string
+}
 
-variable "sns_topic_name" {}
+variable "sns_topic_name" {
+  description = "SNS topic name"
+  type        = string
+}
 
 
 # Optional backend variables if you use remote state:
