eb924ef904dc32f8e5d67f651c164e2ae4c20b2a
Shaffy1 <sweetieshafaw301@gmail.com>
2025-10-01 23:22:15 +0000

    update

--- lambda/get_post.py
+++ lambda/get_post.py
@@ -16,7 +16,8 @@ def lambda_handler(event, context):
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        "Access-Control-Allow-Headers": "Content-Type"
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Credentials": "false"
     }
 
     if not postId:
