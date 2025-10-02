c36b1e91cd654d4073da50e513300cb92088b408
Shaffy1 <sweetieshafaw301@gmail.com>
2025-10-02 01:41:55 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,7 +89,7 @@ resource "aws_cloudfront_distribution" "website" {
 
 # DynamoDB Table
 resource "aws_dynamodb_table" "posts" {
-  name           = "polly-posts"
+  name           = "content_posts"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
   
@@ -101,7 +101,7 @@ resource "aws_dynamodb_table" "posts" {
 
 # S3 Bucket for Audio Files
 resource "aws_s3_bucket" "audio" {
-  bucket = "polly-audio-${random_id.bucket_suffix.hex}"
+  bucket = "audio-storage-bucket-unique-2025"
 }
 
 resource "aws_s3_bucket_public_access_block" "audio" {
