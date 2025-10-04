3d54fa2717b6a80e2cf60808b00b998a03268f33
Shaffy1 <sweetieshafaw301@gmail.com>
2025-10-04 22:14:14 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,20 +35,6 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "website" {
-  bucket = aws_s3_bucket.website.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.website.arn}/*"
-    }]
-  })
-  depends_on = [aws_s3_bucket_public_access_block.website]
-}
-
 # Upload website files
 resource "aws_s3_object" "website_files" {
   for_each = {
